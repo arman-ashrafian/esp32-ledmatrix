e59710b023271a7055a33cd596701f693be0cefe
arman-ashrafian <ashy63@gmail.com>
2018-07-18 11:39:15 -0700

    adding wifi

--- ledmatrix.ino
+++ ledmatrix.ino
@@ -2,7 +2,21 @@
 // ESP32 LED Matrix Program
 
 #define ESP32
+
+// -------- Standard Libraries --------
 #include <PxMatrix.h>
+#include <WiFi.h>
+// #include "time.h"
+
+#include "secret.h"
+
+const char* ssid       = SSID;
+const char* password   = PASSWORD;
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+
 
 // Pins connected to LED Matrix
 #define P_LAT 22
@@ -17,7 +31,7 @@ hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 // this controls the brightness somehow
-uint8_t display_draw_time=2;
+uint8_t display_draw_time=15;
 
 // Contructor for PXMatrix
 PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
@@ -25,8 +39,6 @@ PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
 // my name
 char firstname[] = "ARMAN";
 char lastname[] = "ASH";
-
-
 
 // -------------- Some standard colors ----------------
 uint16_t myRED = display.color565(255, 0, 0);
@@ -64,9 +76,7 @@ void display_update_enable(bool is_enable)
 
 }
 
-// draws grid like dis
-// |
-// |_________
+// draws x & y axis
 void draw_grid(uint16_t color) {
   display.drawLine(0,31,63,31,color); // x-axis
   display.drawLine(0,0,0,32,color);   // y-axis
@@ -76,6 +86,8 @@ void draw_grid(uint16_t color) {
 }
 
 void draw_name(uint16_t color) {
+  display_update_enable(true);
+
   display.setTextColor(myBLUE);
   display.setTextSize(1);
   display.setCursor(5,2);
@@ -83,27 +95,59 @@ void draw_name(uint16_t color) {
   display.print(firstname);
   delay(1000);
   display.write(' ');
+  display.setTextColor(myGREEN);
   display.print(lastname);
   delay(1000);
 
-  display_update_enable(true);
   yield();
 
 }
 
+tm get_time_struct(uint16_t color)
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  return timeinfo;
+}
+
+void connect_wifi() {
+  // connect to WiFi
+  WiFi.begin(ssid, password);
+  display.setCursor(2,5);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      display.write('x');
+  }
+  display.clearDisplay();
+  display.setCursor(5,2);
+  display.print("WIFI");
+  display.setCursor(2,10);
+  display.print("Connected!");
+  delay(1000);
+
+  display_update_enable(true);
+  yield();
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   display.begin(16);  // 1:16 Scan Rate
 
   // slow update
   display.setFastUpdate(false);
+
+  connect_wifi();
+  delay(2000);
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  draw_date(myRED);
 }
 
 void loop() { 
-  draw_grid(myCYAN);
-  delay(1000);
   display.clearDisplay();
-  delay(1000);
   draw_name(myRED);
-
+  delay(1);
 }
